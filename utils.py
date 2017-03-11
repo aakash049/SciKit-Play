e597ffc8e4a72f37f661f6e3cdcbed09fd4fb699
Aakash Saini <rockmyinboxx@gmail.com>
2017-03-11 10:49:20 +0530

    Added main.py and utils.py
    Added labeling of data

---
+++ utils.py
@@ -0,0 +1,30 @@
+# values for normal and striped H candies
+blue = 0
+blue_s_h = 1
+green = 2
+green_s_h = 3
+orange = 4
+orange_s_h = 5
+purple = 6
+purple_s_h = 7
+red = 8
+red_s_h = 9
+yellow = 10
+yellow_s_h = 11
+chocolate_c = 12
+
+# values for striped V candies
+blue_s_v = 13
+green_s_v = 14
+orange_s_v = 15
+purple_s_v = 16
+red_s_v = 17
+yellow_s_v = 18
+
+# values for wrapped candies
+blue_w = 19
+green_w = 20
+orange_w = 21
+purple_w = 22
+red_w = 23
+yellow_w = 24
