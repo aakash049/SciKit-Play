c51cfc89a40f085c464a2ca9e36045b76d96a344
Aadit <aaditbhojgi@gmail.com>
2017-03-13 15:28:33 +0530

    Update SciKit-Play-GUI.py

--- SciKit-Play-GUI.py
+++ SciKit-Play-GUI.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'project2.ui'
-#
-# Created by: PyQt4 UI code generator 4.11.4
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt4 import QtCore, QtGui
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
